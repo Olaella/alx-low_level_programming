801c0ccf230b430c60281b85904ff5175eae63fe
okeke stella <okorochistella@gmail.com>
2023-07-17 14:01:55 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
  * main - A  program that prints a line using the printf function
- * return: 0; (success on successful execution, non zero on errors).
+ * return: 0; (always success on successful execution, non zero on errors).
  */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-Return: 0; (successful)
+	return (0);
+
 }

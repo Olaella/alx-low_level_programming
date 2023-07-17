5d082b25ad0ca155b898dccf17ae745bf7d0545e
okeke stella <okorochistella@gmail.com>
2023-07-17 15:38:45 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+#include <unistdio.h>
+/**
+ * main - A C program that prints a line to the stardard error.
+ * Return: always 1 success
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n, stdout);
+Return: (1);
